b710af64911b30d3049f25b9dec1a9640a41e1fc
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-09-14 15:09:29 -0400

    update register functions to new epivizr calls

--- R/startMetaviz.R
+++ R/startMetaviz.R
@@ -4,56 +4,56 @@
     app$chart_mgr$.register_available_chart_types(request_data$data)
   })
   
-  app$server$register_action("getHierarchy", function(mgr, msgData, ...) {
-    datasource = msgData$datasourceGroup # TODO: Change to datasource
-    nodeId = msgData$nodeId
-    obj <- mgr$.findDatasource(datasource)
+  app$server$register_action("getHierarchy", function(request_data) {
+    datasource = request_data$datasourceGroup # TODO: Change to datasource
+    nodeId = request_data$nodeId
+    obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
     obj$getHierarchy(nodeId)
   })
   
-  app$server$register_action("propagateHierarchyChanges", function(mgr, msgData, ...) {
-    datasource = msgData$datasourceGroup # TODO: Change to datasource
-    selection = msgData$selection
-    order = msgData$order
-    obj <- mgr$.findDatasource(datasource)
+  app$server$register_action("propagateHierarchyChanges", function(request_data) {
+    datasource = request_data$datasourceGroup # TODO: Change to datasource
+    selection = request_data$selection
+    order = request_data$order
+    obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
     obj$propagateHierarchyChanges(selection, order)
   })
   
-  app$server$register_action("getRows", function(mgr, msgData, ...) {
-    datasource = msgData$datasource
-    seqName = msgData$seqName
-    start = msgData$start
-    end = msgData$end
-    metadata = msgData$metadata
+  app$server$register_action("getRows", function(request_data) {
+    datasource = request_data$datasource
+    seqName = request_data$seqName
+    start = request_data$start
+    end = request_data$end
+    metadata = request_data$metadata
     
-    obj <- mgr$.findDatasource(datasource)
+    obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
     obj$getRows(seqName, start, end, metadata)
   })
   
-  app$server$register_action("getValues", function(mgr, msgData, ...) {
-    datasource = msgData$datasource
-    measurement = msgData$measurement
-    seqName = msgData$seqName
-    start = msgData$start
-    end = msgData$end
+  app$server$register_action("getValues", function(request_data) {
+    datasource = request_data$datasource
+    measurement = request_data$measurement
+    seqName = request_data$seqName
+    start = request_data$start
+    end = request_data$end
     
-    obj <- mgr$.findDatasource(datasource)
+    obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
     obj$getValues(measurement, seqName, start, end)
   })
   
-  app$server$register_action("getSeqInfos", function(mgr, msgData, ...) {
+  app$server$register_action("getSeqInfos", function(request_data) {
     return(list(
       list("metavizr", 0, .Machine$integer.max)
     ))
