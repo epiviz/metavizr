4756d3c92047263d952a5c0cbfba27cd3d972415
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-09-26 15:00:49 -0400

    update aggregate function

--- R/plots.R
+++ R/plots.R
@@ -265,7 +265,7 @@ metavizTransformSelect<-function(obj,fun=rowSums ,control=metavizControl(n=100))
 #' @examples
 #' settings = metavizControl()
 #'
-metavizControl<-function(aggregateAtDepth=3,aggregateFun=function(x) log2(1 + colSums(x)),
+metavizControl<-function(aggregateAtDepth=3,aggregateFun=function(x) colSums(x),
             valuesAnnotationFuns=NULL,maxDepth=4,maxHistory=3,maxValue=NULL,
             minValue=NULL,title="",n=10000,rankFun=sd,norm=TRUE,log=FALSE){
 
