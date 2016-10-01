e7fb2bad102f5b9f58ec585263ec3b4e03534b84
Hector Corrada Bravo <hcorrada@gmail.com>
2016-10-01 16:18:30 -0400

    Simplify for submission
    - Add pkgs to Imports file in Description
    - Move the plot functions and documentation out of the way for now
    - Add work directory to the R build ignore file
    - move metavizControl to its own file
    - remove data that is not supposed to be here

--- work/staging/plots.R
+++ work/staging/plots.R
@@ -244,36 +244,7 @@ metavizTransformSelect<-function(obj,fun=rowSums ,control=metavizControl(n=100))
   # return(obj)
   NULL
 }
-#' metavizr settings
-#'
-#' Default settings for the various plotting functions in metavizr.
-#'
-#' @param aggregateAtDepth Level of the tree to aggregate counts at by default.
-#' @param aggregateFun Function to aggregate counts by at the aggregateAtDepth level.
-#' @param valuesAnnotationFuns Function for error bars.
-#' @param maxDepth Level of the tree to display by default in icicle view.
-#' @param maxHistory Value for caching.
-#' @param maxValue Maximum value to display.
-#' @param minValue Minimum value to display.
-#' @param title title.
-#' @param n Number of OTUs to include in ranking.
-#' @param rankFun Ranking function - single vector function.
-#' @param norm Normalize MRexperiment object.
-#' @param log Log tranformation of MRexperiment object.
-#' @return List of setting parameters.
-#' @seealso \code{\link{metavizControl}} \code{\link{metavizRank}} \code{\link{metavizOptimize}}
-#' @examples
-#' settings = metavizControl()
-#'
-#' @export
-metavizControl<-function(aggregateAtDepth=3,aggregateFun=function(x) colSums(x),
-            valuesAnnotationFuns=NULL,maxDepth=4,maxHistory=3,maxValue=NULL,
-            minValue=NULL,title="",n=10000,rankFun=sd,norm=TRUE,log=FALSE){
 
-  list(aggregateAtDepth=aggregateAtDepth,aggregateFun=aggregateFun,valuesAnnotationFuns=valuesAnnotationFuns,
-            maxDepth=maxDepth,maxHistory=maxHistory,maxValue=maxValue,minValue=minValue,title=title,
-            n=n,rankFun=sd,norm=norm,log=log)
-}
 #' Check appropriate values
 #'
 #' Ensure options sent to addMeasurements are reasonable.
