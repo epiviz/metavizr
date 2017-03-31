a8ccb8b23c98e9c951009684326fa94ffb8ae86b
Justin Wagner <jmwagner@umd.edu>
2017-03-31 12:48:38 -0400

    Updating vignette and documentation for metavizControl

--- R/metavizControl.R
+++ R/metavizControl.R
@@ -1,6 +1,6 @@
 #' metavizr settings
 #'
-#' Default settings for the various plotting functions in metavizr.
+#' Default settings for operating over MRexperiment object.
 #'
 #' @param aggregateAtDepth Level of the tree to aggregate counts at by default.
 #' @param aggregateFun Function to aggregate counts by at the aggregateAtDepth level.
