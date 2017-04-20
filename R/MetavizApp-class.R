72dc73a0d9bec9929349a11403f188d4a82927ab
Hector Corrada Bravo <hcorrada@gmail.com>
2017-04-20 03:38:37 +0000

    remove navigate & slideshow methods, update documentation
    From: Jayaram Kancherla <jayaram.kancherla@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metavizr@128922 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MetavizApp-class.R
+++ R/MetavizApp-class.R
@@ -23,49 +23,48 @@ MetavizApp <- setRefClass("MetavizApp",
 )
 
 # # navigation methods
-MetavizApp$methods(
-  navigate=function(node) {
-    'Navigate to a given node on the metaviz app.'
-    if (.self$is_server_closed()) {
-      return(invisible())
-    }
-    
-    callback <- function(response_data) {
-      invisible()
-    }
-    request_data=list(action="navigate",
-                      range=epivizrServer::json_writer(
-                        list(seqName="metavizr",start=node$leafIndex(),end=(node$leafIndex() + node$nleaves()))))
-    .self$server$send_request(request_data, callback)
-  },
-  slideshow=function(nodes, n=length(nodes), .callback=NULL) {
-    'Navigate on metaviz app successively to given nodes.
-    
-    \\describe{
-    \\item{nodes}{An object of class indicating
-    set of nodes to navigate in epiviz app.}
-    \\item{n}{(integer) The number of regions in \\code{nodes} to navigate to.}
-    \\item{.callback}{(function) function to call after navigating to each region. Used for testing purposes.}
-    }'
-
-    n <- min(n, length(nodes))
-    ind <- seq(len=n)
-    chr <- 'metavizr'
-    start <- nodes[ind]$leafIndex()
-    end <- (nodes[ind]$leafIndex() + nodes[ind]$nleaves())
-    for (i in ind) {
-      .self$navigate(chr=chr[i], start=start[i], end=end[i])
-      if (!is.null(.callback) && is.function(.callback)) {
-        .callback(chr[i], start[i], end[i])
-      } else {
-        if (.self$server$is_interactive()) {
-          cat("Node", i, "of", n, ". Press key to continue (ESC to stop)...\n")
-          readLines(n=1)
-        }
-      }
-      tryCatch(.self$server$service(), interrupt=function(int) invisible())
-    }
-    invisible()
-  }
-
-)
+# MetavizApp$methods(
+#   navigate=function(node) {
+#     'Navigate to a given node on the metaviz app.'
+#     if (.self$is_server_closed()) {
+#       return(invisible())
+#     }
+#     
+#     callback <- function(response_data) {
+#       invisible()
+#     }
+#     request_data=list(action="navigate",
+#                       range=epivizrServer::json_writer(
+#                         list(seqName="metavizr",start=node$leafIndex(),end=(node$leafIndex() + node$nleaves()))))
+#     .self$server$send_request(request_data, callback)
+#   },
+#   slideshow=function(nodes, n=length(nodes), .callback=NULL) {
+#     'Navigate on metaviz app successively to given nodes.
+#     
+#     \\describe{
+#     \\item{nodes}{An object of class indicating
+#     set of nodes to navigate in epiviz app.}
+#     \\item{n}{(integer) The number of regions in \\code{nodes} to navigate to.}
+#     \\item{.callback}{(function) function to call after navigating to each region. Used for testing purposes.}
+#     }'
+# 
+#     n <- min(n, length(nodes))
+#     ind <- seq(len=n)
+#     chr <- 'metavizr'
+#     start <- nodes[ind]$leafIndex()
+#     end <- (nodes[ind]$leafIndex() + nodes[ind]$nleaves())
+#     for (i in ind) {
+#       .self$navigate(chr=chr[i], start=start[i], end=end[i])
+#       if (!is.null(.callback) && is.function(.callback)) {
+#         .callback(chr[i], start[i], end[i])
+#       } else {
+#         if (.self$server$is_interactive()) {
+#           cat("Node", i, "of", n, ". Press key to continue (ESC to stop)...\n")
+#           readLines(n=1)
+#         }
+#       }
+#       tryCatch(.self$server$service(), interrupt=function(int) invisible())
+#     }
+#     invisible()
+#   }
+# )
