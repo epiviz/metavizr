9d1eb6f515d90a5dec568099e79cc0b6e75cc149
Hector Corrada Bravo <hcorrada@gmail.com>
2017-05-19 00:09:30 +0000

    Fixing PCA and add tests
    From: Justin Wagner <jmwagner@umd.edu>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/metavizr@129730 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-EpivizMetagenomicsData_class.R
+++ tests/testthat/test-EpivizMetagenomicsData_class.R
@@ -56,8 +56,41 @@ test_that("getRows", {
 })
 
 #getPCA
+test_that("getPCA", {
+  library(metagenomeSeq)
+  data(mouseData)
+  feature_order <- colnames(fData(mouseData))
+  
+  sampleIds<- c("PM9:20071217", "PM10:20080211")
+  mObj <- metavizr:::EpivizMetagenomicsData(mouseData, feature_order = feature_order)
+  res <- mObj$getPCA(measurement = sampleIds)
+  
+  expected_PM9_20071217 <- c(0.7798891, -0.6259177)
+  expect_equal(expected_PM9_20071217, unname(c(res$data[[1]]$PC1, res$data[[1]]$PC2)), tolerance = .0001)
+  
+  expected_PM10_20080211 <- c(0.6259177, 0.7798891)
+  expect_equal(expected_PM10_20080211, unname(c(res$data[[2]]$PC1, res$data[[2]]$PC2)), tolerance = .0001)
+  
+  expected_pca_variance_explained <- c(0.7798627, 0.5189621)
+  expect_equal(expected_pca_variance_explained, res$pca_variance_explained, tolerance = .0001)
+})
 
 #getAlphaDiversity
+test_that("getPCA", {
+  library(metagenomeSeq)
+  data(mouseData)
+  feature_order <- colnames(fData(mouseData))
+  
+  sampleIds<- c("PM9:20071217", "PM10:20080211")
+  mObj <- metavizr:::EpivizMetagenomicsData(mouseData, feature_order = feature_order)
+  res <- mObj$getAlphaDiversity(measurement = sampleIds)
+  
+  expected_PM9_20071217 <- 4.589864
+  expect_equal(expected_PM9_20071217, res$data[[1]]$alphaDiversity, tolerance = .0001)
+  
+  expected_PM10_20080211 <- 3.838823
+  expect_equal(expected_PM10_20080211, res$data[[2]]$alphaDiversity, tolerance = .0001)
+})
 
 #searchTaxonomy
 test_that("searchTaxonomy", {
