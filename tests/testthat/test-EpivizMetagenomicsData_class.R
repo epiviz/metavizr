5bbd6cabd9037e500d1cbe49d1418b4e80d136c0
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-10-28 21:51:07 -0400

    update documentation, fixing examples, updating documentation

--- tests/testthat/test-EpivizMetagenomicsData_class.R
+++ tests/testthat/test-EpivizMetagenomicsData_class.R
@@ -133,6 +133,6 @@ test_that("getValuesInnerNodes", {
   res <- mObj$getValues(measurements = sampleId)
   
   expected <- c(0.0 , 34.1386632, 0.0, 4.9910581, 0.0, 1487.7701940, 3.2804836, 0.4248467, 0.0, 1561.4404797, 0.0, 0.0, 8.6380271, 0.0, 3.7638340, 0.0, 0.0, 0.0, 0.0, 12.3146400, 0.0, 0.0, 120.0305245, 0.3565092, 0.4894787, 0.0, 0.0, 0.0, 0.0)
-  diff_reuslt <- setdiff(round(unname(res[[sampleId]]), digits=3), round(expected,digits=3))
-  expect_equal(length(diff_reuslt), 0)
+  diff_result <- setdiff(round(unname(res[[sampleId]]), digits=3), round(expected,digits=3))
+  expect_equal(length(diff_result), 0)
 })
