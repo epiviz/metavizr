887025dcb5eddb590e86e32b4d1126140333403a
Justin Wagner <jmwagner@umd.edu>
2018-02-05 21:42:11 -0500

    Adding function for replacing NA values in fData

--- R/MetavizGraph-class.R
+++ R/MetavizGraph-class.R
@@ -149,34 +149,9 @@ MetavizGraph <- setRefClass("MetavizGraph",
       fData(obj_in) = fd
                                
       replacing_na_obj_fData <- fData(obj_in)[,feature_order]
-      for(i in seq(1, length(feature_order))){
-        na_indices <- which(is.na(replacing_na_obj_fData[,feature_order[i]]))
-        for(j in seq(1, length(na_indices))){
-          if(i > 1) {
-            replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], replacing_na_obj_fData[,feature_order[1]][na_indices[j]], sep="_")
-          } else {
-            replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], sep="_")
-          }
-        }
-        na_indices <- which(replacing_na_obj_fData[,feature_order[i]] == "NA")
-        for(j in seq(1, length(na_indices))){
-          if(i > 1){ 
-            replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], replacing_na_obj_fData[,feature_order[1]][na_indices[j]], sep="_")
-          } else{
-            replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], sep="_")
-          }
-        }
-        null_indices <- which(replacing_na_obj_fData[,feature_order[i]] == "NULL")
-        for(j in seq(1, length(null_indices))){
-          if(i > 1){ 
-            replacing_na_obj_fData[,feature_order[i]][null_indices[j]] <- paste("Not_Annotated", feature_order[i], replacing_na_obj_fData[,feature_order[1]][null_indices[j]], sep="_")
-          } else{
-            replacing_na_obj_fData[,feature_order[i]][null_indices[j]] <- paste("Not_Annotated", feature_order[i], sep="_")
-          }
-        }
-      }
-                       
-      obj_fData <- as.data.table(replacing_na_obj_fData)
+
+      nas_replaced <- replaceNAFeatures(replacing_na_obj_fData, feature_order)
+      obj_fData <- as.data.table(nas_replaced)
       cols <- feature_order[1:length(feature_order)-1]
       order <- rep(1, length(feature_order)-1)
       ordered_fData <- setorderv(obj_fData, cols = cols, order = order)
--- R/utils.R
+++ R/utils.R
@@ -33,3 +33,43 @@ generateSelection = function(feature_names, aggregation_level, selection_type, f
   fSelection$selectionType <- selection_type
   return(fSelection)
 }
+
+#' Method to replace NA or null feature labels with Not_Annotated_hierarchy-level
+#'
+#' @param replacing_na_obj_fData fData from MRexperiment object to replace NA or null
+#' @param feature_order Order of features
+#' @return hierarchy with NA or null feature labels replaced
+#' @export
+
+replaceNAFeatures = function(replacing_na_obj_fData, feature_order) {
+  
+  for(i in seq(1, length(feature_order))){
+    na_indices <- which(is.na(replacing_na_obj_fData[,feature_order[i]]))
+    for(j in seq(1, length(na_indices))){
+      if(i > 1) {
+        replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], replacing_na_obj_fData[,feature_order[1]][na_indices[j]], sep="_")
+      } else {
+        replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], sep="_")
+      }
+    }
+    na_indices <- which(replacing_na_obj_fData[,feature_order[i]] == "NA")
+    for(j in seq(1, length(na_indices))){
+      if(i > 1){ 
+        replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], replacing_na_obj_fData[,feature_order[1]][na_indices[j]], sep="_")
+      } else{
+        replacing_na_obj_fData[,feature_order[i]][na_indices[j]] <- paste("Not_Annotated", feature_order[i], sep="_")
+      }
+    }
+    null_indices <- which(replacing_na_obj_fData[,feature_order[i]] == "NULL")
+    for(j in seq(1, length(null_indices))){
+      if(i > 1){ 
+        replacing_na_obj_fData[,feature_order[i]][null_indices[j]] <- paste("Not_Annotated", feature_order[i], replacing_na_obj_fData[,feature_order[1]][null_indices[j]], sep="_")
+      } else{
+        replacing_na_obj_fData[,feature_order[i]][null_indices[j]] <- paste("Not_Annotated", feature_order[i], sep="_")
+      }
+    }
+  }
+  
+  replacing_na_obj_fData
+  return(replacing_na_obj_fData)
+}
